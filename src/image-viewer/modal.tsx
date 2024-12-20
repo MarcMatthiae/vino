ab6ea9019b05cc6b28ded0d8aaec6afa0dec0ec2
MarcMatthiae <marc.matthiae@union-investment.de>
2024-12-20 18:10:58 +0100

    Revert modal madness

--- src/image-viewer/modal.tsx
+++ src/image-viewer/modal.tsx
@@ -16,32 +16,6 @@ export const ImageModal = ({
   nextImage,
   prevImage,
 }: ImageModalProps) => {
-  const scrollPositionRef = useRef(0);
-
-  useEffect(() => {
-    if (isOpen) {
-      // Save the current scroll position
-      scrollPositionRef.current = window.scrollY;
-
-      // Prevent background scrolling
-      document.body.style.position = "fixed";
-      document.body.style.width = "100%";
-    } else {
-      // Restore scroll position after modal closes
-      document.body.style.position = "";
-      document.body.style.top = "";
-
-      // Restore the scroll position using the saved value in the ref
-      window.scrollTo(0, scrollPositionRef.current);
-    }
-
-    // Cleanup when the component unmounts
-    return () => {
-      document.body.style.position = "";
-      document.body.style.top = "";
-    };
-  }, [isOpen]);
-
   if (!isOpen) return null; // Don't render the modal if it's not open
 
   return (
