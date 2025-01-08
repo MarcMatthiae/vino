cead77a24542d7855fc0d4542454799d0ef4231f
MarcMatthiae <marc.matthiae@union-investment.de>
2025-01-08 15:31:42 +0100

    Add evaluation

--- src/wine-list/wine-list.tsx
+++ src/wine-list/wine-list.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router";
 import { Wine } from "../wine/wine";
 import "./wine-list.css";
+import { useEffect, useState } from "react";
 
 export interface WineListProps {
   wines: Wine[];
@@ -8,27 +9,72 @@ export interface WineListProps {
 
 export const WineList = ({ wines }: WineListProps) => {
   const navigate = useNavigate();
+
+  const [wineEvaluations, setWineEvaluations] = useState<
+    Array<{ [key: string]: number }>
+  >([]);
+
+  // Load evaluations from localStorage
+  useEffect(() => {
+    const storedEvaluations = JSON.parse(
+      localStorage.getItem("wineEvaluations") || "[]"
+    );
+    setWineEvaluations(storedEvaluations);
+  }, []);
+
+  // Helper to find evaluation for a specific wineId
+  const getEvaluationForWine = (wineId: number) => {
+    const evaluationEntry = wineEvaluations.find((entry) => entry[wineId]);
+    return evaluationEntry ? evaluationEntry[wineId] : null;
+  };
+
   return (
     <div className="wine-list">
-      <ul className=" @[1024px]/main:w-2/3 w-full m-auto">
-        {wines.map((wine) => (
-          <li
-            onClick={() => navigate(`/wine/${wine.id}`)}
-            key={wine.id}
-            className="bg-stone-200  hover:bg-emerald-500 text-stone-700 bg-opacity-75 hover:text-stone-100 cursor-pointer p-4 list-none grid grid-cols-12 gap-2 shadow-slate-500 shadow-sm rounded-md mb-2 last:mb-0"
-          >
-            <div className="flex justify-start items-center col-span-9">
-              <h2 className="text-left text-xl font-extralight">{wine.name}</h2>
-            </div>
-            <div className="col-span-3 flex justify-end">
-              <img
-                className="max-h-[200px] h-[200px] object-contain"
-                src={"/assets/wines/" + wine.key + "/bottle.png"}
-                alt={`${wine.name} wine`}
-              />
-            </div>
-          </li>
-        ))}
+      <ul className="@[1024px]/main:w-2/3 w-full m-auto">
+        {wines.map((wine) => {
+          const evaluation = getEvaluationForWine(wine.id); // Get evaluation for this wine
+
+          return (
+            <li
+              onClick={() => navigate(`/wine/${wine.id}`)}
+              key={wine.id}
+              className="bg-stone-200 hover:bg-emerald-500 text-stone-700 bg-opacity-75 hover:text-stone-100 cursor-pointer p-4 list-none grid grid-cols-12 gap-2 shadow-slate-500 shadow-sm rounded-md mb-2 last:mb-0 items-center"
+            >
+              <div className="flex flex-col justify-start items-start col-span-2">
+                {evaluation ? (
+                  <div className="mt-1 text-sm text-stone-600 border border-stone-600 h-40 w-40 rounded-md flex items-center justify-center">
+                    <p className="text-[50px]">
+                      {evaluation === 6 && "\u{1F92E}"}
+                      {evaluation === 5 && "\u{1F922}"}
+                      {evaluation === 4 && "\u{1F974}"}
+                      {evaluation === 3 && "\u{1F937}"}
+                      {evaluation === 2 && "\u{1F60D}"}
+                      {evaluation === 1 && "\u{1F680}"}
+                    </p>
+                  </div>
+                ) : (
+                  <div className="mt-1 text-sm text-stone-600 border border-stone-600 h-40 w-40 rounded-md flex items-center justify-center">
+                    <p className="text-[14px] text-center">
+                      Bewertung ausstehend
+                    </p>
+                  </div>
+                )}
+              </div>
+              <div className="flex flex-col justify-start items-start col-span-7">
+                <h2 className="text-left text-xl font-extralight">
+                  {wine.name}
+                </h2>
+              </div>
+              <div className="col-span-3 flex justify-end">
+                <img
+                  className="max-h-[200px] h-[200px] object-contain"
+                  src={"/assets/wines/" + wine.key + "/bottle.png"}
+                  alt={`${wine.name} wine`}
+                />
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
--- src/wine/wine.tsx
+++ src/wine/wine.tsx
@@ -1,8 +1,8 @@
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
 import { Winery, WineryData } from "../winery/winery";
 import { CountryLogo } from "../ui-components/country-logo";
 import { HorizontalList } from "../ui-components/horizontal-list";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ImageModal from "../image-viewer/modal";
 
 export interface WineProps {
@@ -47,107 +47,190 @@ export const Wine = ({ wines, wineries, showOverlay }: WineProps) => {
     setTastesVisible((prevState) => !prevState);
   };
 
+  const [selectedEvaluation, setSelectedEvaluation] = useState<number | null>(
+    null
+  );
+
+  // Helper function to find an evaluation for the current wineId
+  const getWineEvaluation = (
+    wineEvaluations: Array<{ [key: string]: number }>,
+    wineId: string
+  ) => {
+    const evaluationEntry = wineEvaluations.find(
+      (entry) => entry[wineId] !== undefined
+    );
+    return evaluationEntry ? evaluationEntry[wineId] : null;
+  };
+
+  useEffect(() => {
+    const storedEvaluations = JSON.parse(
+      localStorage.getItem("wineEvaluations") || "[]"
+    );
+    const evaluation = getWineEvaluation(storedEvaluations, wineId);
+    if (evaluation) {
+      setSelectedEvaluation(evaluation);
+    }
+  }, [wineId]);
+
+  const handleEvaluationClick = (evaluation: number) => {
+    setSelectedEvaluation(evaluation);
+
+    // Get existing evaluations or create a new array
+    const storedEvaluations: Array<{ [key: string]: number }> = JSON.parse(
+      localStorage.getItem("wineEvaluations") || "[]"
+    );
+
+    // Remove existing evaluation for this wineId
+    const updatedEvaluations = storedEvaluations.filter(
+      (entry) => !entry[wineId]
+    );
+
+    // Add the new evaluation
+    updatedEvaluations.push({ [wineId]: evaluation });
+
+    // Save back to localStorage
+    localStorage.setItem("wineEvaluations", JSON.stringify(updatedEvaluations));
+  };
+
   return (
     wine && (
-      <div className="wine-portrait bg-stone-400 bg-no-repeat bg-cover w-full h-full px-2 py-8 min-h-dvh">
-        <div className="wine @[1024px]/main:w-[95%] w-full m-auto bg-stone-100 bg-opacity-75 text-stone-700 wine p-4 list-none grid grid-cols-12 gap-2 shadow-slate-500 shadow-sm rounded-md mb-2 last:mb-0">
-          <div className="flex flex-col justify-start items-start col-span-12">
-            <div className="flex items-center mb-4 gap-4">
-              <CountryLogo
-                countryIso2={winery?.country}
-                countryName={winery?.country_name}
-              ></CountryLogo>
-              <h2 className="text-left text-3xl font-bold text-stone-700">
-                {wine.name}
-              </h2>
+      <>
+        <div className="wine-portrait bg-stone-400 bg-no-repeat bg-cover w-full h-full px-2 py-8 min-h-dvh">
+          <div className="wine @[1024px]/main:w-[95%] w-full m-auto bg-stone-100 bg-opacity-75 text-stone-700 wine p-4 list-none grid grid-cols-12 gap-2 shadow-slate-500 shadow-sm rounded-md mb-2 last:mb-0">
+            <div className="flex flex-col justify-start items-start col-span-12">
+              <div className="flex items-center mb-4 gap-4">
+                <CountryLogo
+                  countryIso2={winery?.country}
+                  countryName={winery?.country_name}
+                ></CountryLogo>
+                <h2 className="text-left text-3xl font-bold text-stone-700">
+                  {wine.name}
+                </h2>
+              </div>
+            </div>
+            <div className="flex flex-col justify-start items-start col-span-9 gap-4">
+              <p className="text-left text-[16px]">{wine.description}</p>
+              <ul className="w-4/5">
+                {wine.quickfacts?.map((quickfact) => {
+                  return (
+                    <li className="grid grid-cols-12 border-b border-stone-700 p-4">
+                      <div className="@[1024px]/main:col-span-3 col-span-12 font-bold">
+                        {quickfact.label}
+                      </div>
+                      <div className="@[1024px]/main:col-span-9 col-span-12 @[1024px]/main:text-right">
+                        {quickfact.value}
+                      </div>
+                    </li>
+                  );
+                })}
+                <li className="grid grid-cols-12 border-b border-stone-700 p-4">
+                  <div className="@[1024px]/main:col-span-3 col-span-12 font-bold">
+                    Vinifikation
+                  </div>
+                  <div className="@[1024px]/main:col-span-9 col-span-12 @[1024px]/main:text-right">
+                    <HorizontalList items={wine.vinification}></HorizontalList>
+                  </div>
+                </li>
+                <li className="grid grid-cols-12 border-b border-stone-700 p-4">
+                  <div className="@[1024px]/main:col-span-3 col-span-12 font-bold">
+                    Geschmack
+                  </div>
+                  {tastesVisible ? (
+                    <div className="@[1024px]/main:col-span-9 col-span-12 flex @[1024px]/main:flex-row flex-col gap-3 justify-end items-center">
+                      <HorizontalList items={wine.tastes}></HorizontalList>
+                      <button
+                        className="bg-orange-200 p-2 rounded-md border-stone-700 border"
+                        onClick={() => handleOnClick()}
+                      >
+                        Ausblenden
+                      </button>
+                    </div>
+                  ) : (
+                    <div className="@[1024px]/main:col-span-9 col-span-12 flex @[1024px]/main:flex-row flex-col gap-3 justify-end  items-center">
+                      <div className="flex">
+                        {[...Array(wine.tastes?.length).keys()]?.map(
+                          (index) => {
+                            return (
+                              <img
+                                className="h-8"
+                                src="/assets/favicon.png"
+                              ></img>
+                            );
+                          }
+                        )}
+                      </div>
+                      <button
+                        className="bg-emerald-300 p-2 rounded-md border-stone-700 border"
+                        onClick={() => handleOnClick()}
+                      >
+                        Anzeigen ({wine.tastes?.length})
+                      </button>
+                    </div>
+                  )}
+                </li>
+                <li className="grid grid-cols-12 border-b border-stone-700 p-4">
+                  <div className="@[1024px]/main:col-span-3 col-span-12 font-bold">
+                    Besonderheiten
+                  </div>
+                  <div className="@[1024px]/main:col-span-9 col-span-12 @[1024px]/main:text-right">
+                    <HorizontalList
+                      items={wine.besonderheiten}
+                    ></HorizontalList>
+                  </div>
+                </li>
+                <li className="grid grid-cols-12 border-b border-stone-700 p-4">
+                  <div className="@[1024px]/main:col-span-3 col-span-12 font-bold">
+                    Besonderheiten Anbaugebiet
+                  </div>
+                  <div className="@[1024px]/main:col-span-9 col-span-12 @[1024px]/main:text-right">
+                    <HorizontalList
+                      items={wine.besonderheiten_anbaugebiet}
+                    ></HorizontalList>
+                  </div>
+                </li>
+              </ul>
+            </div>
+
+            <div className="col-span-3 flex justify-center">
+              <img
+                className="@[1024px]/main:max-h-[800px] @[1024px]/main:h-[800px] max-h-[200px] h-[200px] object-contain col-span-3"
+                src={"/assets/wines/" + wine.key + "/bottle.png"}
+                alt={`${wine.name} wine`}
+              />
             </div>
           </div>
-          <div className="flex flex-col justify-start items-start col-span-9 gap-4">
-            <p className="text-left text-[16px]">{wine.description}</p>
-            <ul className="w-4/5">
-              {wine.quickfacts?.map((quickfact) => {
-                return (
-                  <li className="grid grid-cols-12 border-b border-stone-700 p-4">
-                    <div className="@[1024px]/main:col-span-3 col-span-12 font-bold">
-                      {quickfact.label}
-                    </div>
-                    <div className="@[1024px]/main:col-span-9 col-span-12 @[1024px]/main:text-right">
-                      {quickfact.value}
-                    </div>
-                  </li>
-                );
-              })}
-              <li className="grid grid-cols-12 border-b border-stone-700 p-4">
-                <div className="@[1024px]/main:col-span-3 col-span-12 font-bold">
-                  Vinifikation
-                </div>
-                <div className="@[1024px]/main:col-span-9 col-span-12 @[1024px]/main:text-right">
-                  <HorizontalList items={wine.vinification}></HorizontalList>
-                </div>
-              </li>
-              <li className="grid grid-cols-12 border-b border-stone-700 p-4">
-                <div className="@[1024px]/main:col-span-3 col-span-12 font-bold">
-                  Geschmack
-                </div>
-                {tastesVisible ? (
-                  <div className="@[1024px]/main:col-span-9 col-span-12 flex @[1024px]/main:flex-row flex-col gap-3 justify-end items-center">
-                    <HorizontalList items={wine.tastes}></HorizontalList>
-                    <button
-                      className="bg-orange-200 p-2 rounded-md border-stone-700 border"
-                      onClick={() => handleOnClick()}
-                    >
-                      Ausblenden
-                    </button>
-                  </div>
-                ) : (
-                  <div className="@[1024px]/main:col-span-9 col-span-12 flex @[1024px]/main:flex-row flex-col gap-3 justify-end  items-center">
-                    <div className="flex">
-                      {[...Array(wine.tastes?.length).keys()]?.map((index) => {
-                        return (
-                          <img className="h-8" src="/assets/favicon.png"></img>
-                        );
-                      })}
-                    </div>
-                    <button
-                      className="bg-emerald-300 p-2 rounded-md border-stone-700 border"
-                      onClick={() => handleOnClick()}
-                    >
-                      Anzeigen ({wine.tastes?.length})
-                    </button>
-                  </div>
-                )}
-              </li>
-              <li className="grid grid-cols-12 border-b border-stone-700 p-4">
-                <div className="@[1024px]/main:col-span-3 col-span-12 font-bold">
-                  Besonderheiten
-                </div>
-                <div className="@[1024px]/main:col-span-9 col-span-12 @[1024px]/main:text-right">
-                  <HorizontalList items={wine.besonderheiten}></HorizontalList>
-                </div>
-              </li>
-              <li className="grid grid-cols-12 border-b border-stone-700 p-4">
-                <div className="@[1024px]/main:col-span-3 col-span-12 font-bold">
-                  Besonderheiten Anbaugebiet
-                </div>
-                <div className="@[1024px]/main:col-span-9 col-span-12 @[1024px]/main:text-right">
-                  <HorizontalList
-                    items={wine.besonderheiten_anbaugebiet}
-                  ></HorizontalList>
-                </div>
-              </li>
-            </ul>
-          </div>
-
-          <div className="col-span-3 flex justify-center">
-            <img
-              className="@[1024px]/main:max-h-[800px] @[1024px]/main:h-[800px] max-h-[200px] h-[200px] object-contain col-span-3"
-              src={"/assets/wines/" + wine.key + "/bottle.png"}
-              alt={`${wine.name} wine`}
-            />
+          {winery && (
+            <Winery winery={winery} showOverlay={showOverlay}></Winery>
+          )}
+        </div>
+        <div className="h-20 fixed bottom-0 box-border border-t z-50 w-full @[1024px]/main:px-16 flex justify-start items-center shadow-[0px_-4px_6px_rgba(0,0,0,0.1)] bg-stone-200 py-2">
+          <div className="m-auto flex items-center gap-4">
+            <div>Jetzt bewerten:</div>
+            <div className="m-auto flex items-center gap-2">
+              {[6, 5, 4, 3, 2, 1].map((evaluation) => (
+                <button
+                  key={evaluation}
+                  className={`p-2 w-12 h-12 rounded-md border-stone-700 border text-[30px] flex items-center justify-center ${
+                    selectedEvaluation === evaluation
+                      ? "bg-emerald-400 text-white"
+                      : "hover:bg-emerald-400 hover:text-white"
+                  }`}
+                  onClick={() => handleEvaluationClick(evaluation)}
+                >
+                  <p>
+                    {evaluation === 6 && "\u{1F92E}"}
+                    {evaluation === 5 && "\u{1F922}"}
+                    {evaluation === 4 && "\u{1F974}"}
+                    {evaluation === 3 && "\u{1F937}"}
+                    {evaluation === 2 && "\u{1F60D}"}
+                    {evaluation === 1 && "\u{1F680}"}
+                  </p>
+                </button>
+              ))}
+            </div>
           </div>
         </div>
-        {winery && <Winery winery={winery} showOverlay={showOverlay}></Winery>}
-      </div>
+      </>
     )
   );
 };
